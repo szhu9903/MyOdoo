80e00b613e117100499e31079210929f127c71c9
15994092160 <1781082307@qq.com>
2020-05-31 21:08:53 +0800

    user pwd 加密

--- myodoo/models/my_user.py
+++ myodoo/models/my_user.py
@@ -9,5 +9,28 @@ class Myuser(models.Model):
     _order = 'fname asc'
 
     fname = fields.Char('用户姓名',track_visibility='onchange')
+    fuser_code = fields.Char('账号',track_visibility='onchange')
+    fuser_pwd = fields.Char('密码',track_visibility='onchange')
+
+    @api.model
+    def create(self, vals):
+        pwd = vals.get('fuser_pwd')
+        if pwd:
+            hl = hashlib.md5(pwd.encode(encoding='utf-8'))
+            vals['fuser_pwd'] = hl.hexdigest()
+        res = super(Myuser,self).create(vals)
+        return res
+
+    def write(self, vals):
+        pwd = vals.get('fuser_pwd') or ''
+        if pwd:
+            hl = hashlib.md5(pwd.encode(encoding='utf-8'))
+            vals['fuser_pwd'] = hl.hexdigest()
+        result = super(Myuser,self).write(vals)
+        return result
+
+
+
+
 
 
