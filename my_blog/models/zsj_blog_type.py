21053557044b52c82f12029b6734b5b616e18905
15994092160 <1781082307@qq.com>
2020-07-21 21:52:04 +0800

    form action

--- my_blog/models/zsj_blog_type.py
+++ my_blog/models/zsj_blog_type.py
@@ -14,3 +14,24 @@ class ZsjBlogType(models.Model):
     zcreate_by = fields.Many2one('zsj.blog.user','创建人')
     zdel_flag = fields.Integer('删除标志',default=0)
 
+
+    def get_zsj_blog(self):
+        sql = """
+        select id from zsj_blog where zblog_type='%s'
+        """%self.id
+        self._cr.execute()
+        res = self._cr_fetchall()
+        blog_list = [blog_id[0] for blog_id in res]
+        formview_ref = self.env.ref('my_blog.view_my_blog_form',False)
+        treeview_ref = self.env.ref('my_blog.view_my_blog_tree',False)
+        return {
+            'name': '所有文章',
+            'view_type': 'form',
+            'view_model': 'form,tree',
+            'res_model': 'zsj.blog',
+            'domain':"[('zblog_type','in',%s)]"%blog_list,
+            'views':[(treeview_ref and treeview_ref.id or False,'tree'),
+                     (formview_ref and formview_ref.id or False,'form')],
+            'type':'ir.actions.act_window',
+            'target':'new'
+        }
